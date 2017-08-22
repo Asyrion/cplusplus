5aac7fad1e5a705c1886eecec21ed5c4e05bc2dc
Roman Harms <r.harms@md-systemhaus.de>
2017-08-22 13:48:50 +0200

    Added some testing files

---
+++ demo2.c
@@ -0,0 +1,23 @@
+// Both notations possible
+#include <iostream>
+
+// Not needed - namespace std is standard
+using namespace std;
+// #include "iostream"
+
+
+// demo2.C - Sample C++ programm
+
+/**
+ * 
+ * @author Roman Harms
+ */
+int main(void)
+{
+    int i;
+    std::cout << "Please enter an integer value:";
+    std::cin >> i;
+    std::cout << "The value you entered is " << i;
+    std::cout << " and its double is " << i*2 << "\n";
+    return 0;
+}
